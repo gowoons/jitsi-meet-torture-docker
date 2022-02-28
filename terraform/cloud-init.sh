99f096490076413e62344d54119d4e601e806ca6
Justin Béra <me@just1not2.org>
2022-02-28 14:00:12 +0100

    :sparkles:(cloud-init) add cloud-init script
    In order to provision our build instance, we need
    to install docker, docker-compose and jitsi-meet-
    torture in the cloud-init script

--- terraform/cloud-init.sh
+++ terraform/cloud-init.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
-echo "Hello World. The time is now $(date -R)!" | tee /root/output.txt
-sleep 1000
+
+# Install Docker
+apt update
+apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/docker.gpg --import
+chmod 644 /etc/apt/trusted.gpg.d/docker.gpg
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt -y install docker-ce docker-ce-cli containerd.io
+
+# Install Docker-Compose
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+chmod +x docker-compose-linux-x86_64
+mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+
+# Configure Jitsi-Meet-Torture
+git clone https://github.com/gowoons/jitsi-meet-torture-docker
+cd jitsi-meet-torture-docker #/docker
+cp .env.template .env
+docker-compose build
+docker-compose pull
